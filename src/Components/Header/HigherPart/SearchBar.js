5a0d8a22175902f1e548031bbe060bb50b5c8274
ahmadeleiwa <ahmadilawa@gmail.com>
2023-03-12 14:07:53 +0200

    remove button border & change background color to transparent

--- src/Components/Header/HigherPart/SearchBar.js
+++ src/Components/Header/HigherPart/SearchBar.js
@@ -3,7 +3,7 @@ import shape from '../../../assets/Shape.png'
 import './SearchBar.css'
 const SearchBar = props =>{
     return <div className="search-bar">
-        <button className=''><img src={shape} /></button>
+        <button  className=''><img src={shape} /></button>
         <input type={'search'} placeholder='Search' />
     </div>
 }
