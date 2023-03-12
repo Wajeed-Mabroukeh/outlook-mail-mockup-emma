0b179e446311b4b9b55886125f8bdb349981638a
ahmadeleiwa <ahmadilawa@gmail.com>
2023-03-12 14:26:51 +0200

    center the icon

--- src/Components/Header/LowerPart/More.js
+++ src/Components/Header/LowerPart/More.js
@@ -1,6 +1,6 @@
 import moreIcon from  '../../../assets/More.png'
 const More = props =>{
-    return <a href="#"><img src={moreIcon}/></a>
+    return <a href="#" style={{display:'flex'}} ><img src={moreIcon}/></a>
 }
 
 export default More
