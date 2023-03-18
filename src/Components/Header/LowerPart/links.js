2ec82dbd1ff61dd8a0455e9731014261f998c32e
ahmadeleiwa <ahmadilawa@gmail.com>
2023-03-18 14:06:12 +0200

    Adding data links file & chaning in Linklist by using map function

---
+++ src/Components/Header/LowerPart/links.js
@@ -0,0 +1,25 @@
+
+import deleteIcon from '../../../assets/Icon-Arrow-down Copy 7.png'
+import archive from '../../../assets/Icon-Arrow-down Copy 2.png'
+import junk from '../../../assets/Icon-Arrow-down Copy.png'
+import sweep from '../../../assets/Icon-Sweep.png'
+
+import Combined from '../../../assets/Combined Shape-2.png'
+import Categorize from '../../../assets/Combined Shape-1.png'
+import clock from '../../../assets/Icon-Arrow-down Copy 5.png'
+import reset from '../../../assets/Icon/UI/restart-1.png'
+
+
+
+export const links = [
+    { title: "Delete", icon: deleteIcon },
+    { title: "Archive", icon: archive },
+    { title: "Junk", icon: junk },
+    { title: "Sweep", icon: sweep },
+    { title: "Move to", icon: Combined },
+    { title: "Categorize", icon: Categorize },
+    { title: "Categorize", icon: clock },
+
+    { title: "Undo", icon: reset, gray:true },
+
+]
